b18ff476ce9a5b7ed062d933b9912d05ea17807d
grjus <grjus@pm.me>
2021-08-13 11:07:21 +0200

    Initial commit

---
+++ src/main/kotlin/com/traing/grjus/recepieproject/RecepieProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.traing.grjus.recepieproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecepieProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecepieProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/traing/grjus/recepieproject/RecepieProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.traing.grjus.recepieproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecepieProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
