40fe3ca67db8e7833d07497c05e40e5c036adf16
grjus <grjus@pm.me>
2021-08-16 14:58:09 +0200

    Added initialization of the sql database. NOTE:

--- src/main/kotlin/com/traing/grjus/recepieproject/repositories/UnitOfMeasureRepository.kt
+++ src/main/kotlin/com/traing/grjus/recepieproject/repositories/UnitOfMeasureRepository.kt
@@ -3,5 +3,7 @@ package com.traing.grjus.recepieproject.repositories
 import com.traing.grjus.recepieproject.domain.UnitOfMeasure
 import org.springframework.data.repository.CrudRepository
 
-interface UnitOfMeasureRepository:CrudRepository<UnitOfMeasure, Long> {
+
+interface UnitOfMeasureRepository :CrudRepository<UnitOfMeasure, Long>{
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Polish');
+INSERT INTO category (description) VALUES ('Fast Food');
